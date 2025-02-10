29174d8977ac174b88bd2a98508148ec0e4b6e24
shivamrajput18 <shivamsingh80094@gmail.com>
2025-02-10 10:55:39 +0530

    printing Welcome message

---
+++ printingMessage.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class printingMessage {
+    public static void displayMessage(){
+        System.out.println("Welcome to BridgeLabz!");
+    }
+    public static void main(String[] args) {
+      displayMessage();  
+    }
+}
